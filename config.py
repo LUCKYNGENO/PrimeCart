40984b82943eec549d3335bbd6eaef426e130242
LUCKYNGENO <ngenolucky5@gmail.com>
2026-07-20 22:17:26 +0300

    Use environment variables

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
-MYSQL_HOST = "localhost"
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "@Makori321"
-MYSQL_DB = "primecart"
-SECRET_KEY = "PrimeCartSecret123"
+import os
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.getenv("MYSQL_DB")
+MYSQL_PORT = int(os.getenv("MYSQL_PORT", "3306"))
+
+SECRET_KEY = os.getenv("SECRET_KEY")
